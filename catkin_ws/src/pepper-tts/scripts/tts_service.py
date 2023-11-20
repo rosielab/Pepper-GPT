4366114140fbb60e0091cac3fb633ef080cbde48
Ben Ledingham <benledingham1@gmail.com>
2023-11-20 04:45:36 +0000

    tts works, audio input not supported on naoqi_driver v2.9

---
+++ catkin_ws/src/pepper-tts/scripts/tts_service.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import String
+from naoqi_bridge_msgs.msg import AudioBuffer
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + ' LOGGING DATA %s', data)
+    # pub = rospy.Publisher('speech', String, queue_size=1)
+    # pub.publish(String("Received joint data"))
+
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('audio', AudioBuffer, callback)
+
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
